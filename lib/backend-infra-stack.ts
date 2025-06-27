4f15abf07233e96c050d89b3db748ab24de47e29
Ramkumar Paranthaman <parantr@amazon.com>
2025-06-26 20:57:35 -0700

    Fix build errors

--- lib/backend-infra-stack.ts
+++ lib/backend-infra-stack.ts
@@ -20,7 +20,7 @@ export class BackendInfraStack extends cdk.Stack {
     const { prefix } = props;
 
     // Create VPC
-    const vpc = new ec2.Vpc(this, `${prefix}-VPC`, {
+    const vpc = new ec2.Vpc(scope, `${prefix}-VPC`, {
       maxAzs: 2,
       natGateways: 1,
       subnetConfiguration: [
@@ -38,7 +38,7 @@ export class BackendInfraStack extends cdk.Stack {
     });
 
     // Create ECR Repository
-    const ecrRepository = new ecr.Repository(this, `${prefix}-ECR-Repository`, {
+    const ecrRepository = new ecr.Repository(scope, `${prefix}-ECR-Repository`, {
       repositoryName: `${prefix}-backend-app`.toLowerCase(),
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       imageTagMutability: ecr.TagMutability.MUTABLE,
@@ -46,14 +46,14 @@ export class BackendInfraStack extends cdk.Stack {
     });
 
     // Create ECS Cluster
-    const cluster = new ecs.Cluster(this, `${prefix}-ECS-Cluster`, {
+    const cluster = new ecs.Cluster(scope, `${prefix}-ECS-Cluster`, {
       clusterName: `${prefix}-backend-cluster`,
       vpc,
       containerInsights: true,
     });
 
     // Create CloudWatch Log Group
-    const logGroup = new logs.LogGroup(this, `${prefix}-Log-Group`, {
+    const logGroup = new logs.LogGroup(scope, `${prefix}-Log-Group`, {
       logGroupName: `/aws/ecs/${prefix}-backend-app`,
       retention: logs.RetentionDays.ONE_MONTH,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -85,7 +85,6 @@ export class BackendInfraStack extends cdk.Stack {
           },
         },
         loadBalancerName: `${prefix}-ALB`,
-        enableLogging: true,
         cloudMapOptions: {
           name: `${prefix}-backend`,
         },
@@ -123,7 +122,7 @@ export class BackendInfraStack extends cdk.Stack {
     });
 
     // Create SNS Topic for Alarms
-    const alarmTopic = new sns.Topic(this, `${prefix}-Alarm-Topic`, {
+    const alarmTopic = new sns.Topic(scope, `${prefix}-Alarm-Topic`, {
       topicName: `${prefix}-backend-alarms`,
       displayName: `${prefix} Backend Infrastructure Alarms`,
     });
@@ -131,7 +130,7 @@ export class BackendInfraStack extends cdk.Stack {
     // CloudWatch Alarms
 
     // High CPU Utilization Alarm
-    const highCpuAlarm = new cloudwatch.Alarm(this, `${prefix}-High-CPU-Alarm`, {
+    const highCpuAlarm = new cloudwatch.Alarm(scope, `${prefix}-High-CPU-Alarm`, {
       alarmName: `${prefix}-HighCPUUtilization`,
       alarmDescription: 'Alarm when CPU exceeds 80%',
       metric: fargateService.service.metricCpuUtilization({
@@ -146,7 +145,7 @@ export class BackendInfraStack extends cdk.Stack {
     highCpuAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // High Memory Utilization Alarm
-    const highMemoryAlarm = new cloudwatch.Alarm(this, `${prefix}-High-Memory-Alarm`, {
+    const highMemoryAlarm = new cloudwatch.Alarm(scope, `${prefix}-High-Memory-Alarm`, {
       alarmName: `${prefix}-HighMemoryUtilization`,
       alarmDescription: 'Alarm when Memory exceeds 85%',
       metric: fargateService.service.metricMemoryUtilization({
@@ -161,10 +160,10 @@ export class BackendInfraStack extends cdk.Stack {
     highMemoryAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // Application Load Balancer Target Unhealthy Alarm
-    const unhealthyTargetsAlarm = new cloudwatch.Alarm(this, `${prefix}-Unhealthy-Targets-Alarm`, {
+    const unhealthyTargetsAlarm = new cloudwatch.Alarm(scope, `${prefix}-Unhealthy-Targets-Alarm`, {
       alarmName: `${prefix}-UnhealthyTargets`,
       alarmDescription: 'Alarm when there are unhealthy targets behind the load balancer',
-      metric: fargateService.loadBalancer.metricUnhealthyHostCount({
+      metric: fargateService.targetGroup.metricUnhealthyHostCount({
         period: cdk.Duration.minutes(1),
         statistic: 'Average',
       }),
@@ -176,7 +175,7 @@ export class BackendInfraStack extends cdk.Stack {
     unhealthyTargetsAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // HTTP 5xx Error Rate Alarm
-    const http5xxAlarm = new cloudwatch.Alarm(this, `${prefix}-HTTP-5xx-Alarm`, {
+    const http5xxAlarm = new cloudwatch.Alarm(scope, `${prefix}-HTTP-5xx-Alarm`, {
       alarmName: `${prefix}-HTTP5xxErrors`,
       alarmDescription: 'Alarm when HTTP 5xx error rate is high',
       metric: fargateService.loadBalancer.metricHttpCodeTarget(
@@ -194,7 +193,7 @@ export class BackendInfraStack extends cdk.Stack {
     http5xxAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // Response Time Alarm
-    const responseTimeAlarm = new cloudwatch.Alarm(this, `${prefix}-Response-Time-Alarm`, {
+    const responseTimeAlarm = new cloudwatch.Alarm(scope, `${prefix}-Response-Time-Alarm`, {
       alarmName: `${prefix}-HighResponseTime`,
       alarmDescription: 'Alarm when response time exceeds 2 seconds',
       metric: fargateService.loadBalancer.metricTargetResponseTime({
@@ -209,10 +208,16 @@ export class BackendInfraStack extends cdk.Stack {
     responseTimeAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // Task Count Alarm (for when service is down)
-    const taskCountAlarm = new cloudwatch.Alarm(this, `${prefix}-Task-Count-Alarm`, {
+    const taskCountAlarm = new cloudwatch.Alarm(scope, `${prefix}-Task-Count-Alarm`, {
       alarmName: `${prefix}-LowTaskCount`,
       alarmDescription: 'Alarm when running task count is below desired',
-      metric: fargateService.service.metricRunningTaskCount({
+      metric: new cloudwatch.Metric({
+        namespace: 'AWS/ECS',
+        metricName: 'RunningTaskCount',
+        dimensionsMap: {
+          ServiceName: fargateService.service.serviceName,
+          ClusterName: cluster.clusterName,
+        },
         period: cdk.Duration.minutes(1),
         statistic: 'Average',
       }),
@@ -225,37 +230,37 @@ export class BackendInfraStack extends cdk.Stack {
     taskCountAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
 
     // Outputs
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+    new cdk.CfnOutput(scope, 'LoadBalancerDNS', {
       value: fargateService.loadBalancer.loadBalancerDnsName,
       description: 'Load Balancer DNS Name',
       exportName: `${prefix}-LoadBalancerDNS`,
     });
 
-    new cdk.CfnOutput(this, 'ECRRepositoryURI', {
+    new cdk.CfnOutput(scope, 'ECRRepositoryURI', {
       value: ecrRepository.repositoryUri,
       description: 'ECR Repository URI',
       exportName: `${prefix}-ECRRepositoryURI`,
     });
 
-    new cdk.CfnOutput(this, 'ClusterName', {
+    new cdk.CfnOutput(scope, 'ClusterName', {
       value: cluster.clusterName,
       description: 'ECS Cluster Name',
       exportName: `${prefix}-ClusterName`,
     });
 
-    new cdk.CfnOutput(this, 'ServiceName', {
+    new cdk.CfnOutput(scope, 'ServiceName', {
       value: fargateService.service.serviceName,
       description: 'ECS Service Name',
       exportName: `${prefix}-ServiceName`,
     });
 
-    new cdk.CfnOutput(this, 'LogGroupName', {
+    new cdk.CfnOutput(scope, 'LogGroupName', {
       value: logGroup.logGroupName,
       description: 'CloudWatch Log Group Name',
       exportName: `${prefix}-LogGroupName`,
     });
 
-    new cdk.CfnOutput(this, 'SNSTopicArn', {
+    new cdk.CfnOutput(scope, 'SNSTopicArn', {
       value: alarmTopic.topicArn,
       description: 'SNS Topic ARN for Alarms',
       exportName: `${prefix}-SNSTopicArn`,
